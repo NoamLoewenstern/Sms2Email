75f1a3d36d9aa0323991746d6bc68850022b2526
Noam Loewenstern <NoamLoewenstern@gmail.com>
2023-01-12 11:51:50 +0200

    basic app works

---
+++ lib/sign-in-btn.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+// class SignInBtn extends StatefulWidget {
+//   const SignInBtn({Key? key}) : super(key: key);
+
+//   @override
+//   State<SignInBtn> createState() => _SignInBtnState();
+// }
+
+// class _SignInBtnState extends State<SignInBtn> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return // Generated code for this Button Widget...
+// Align(
+//   alignment: AlignmentDirectional(0, 0),
+//   child: B(
+//     onPressed: () {
+//       print('Button pressed ...');
+//     },
+//     text: 'Sign in with Google',
+//     icon: Icon(
+//       Icons.add,
+//       color: Colors.transparent,
+//       size: 20,
+//     ),
+//     options: FFButtonOptions(
+//       width: 230,
+//       height: 44,
+//       color: Colors.white,
+//       textStyle: GoogleFonts.getFont(
+//         'Roboto',
+//         color: Color(0xFF606060),
+//         fontSize: 17,
+//       ),
+//       elevation: 4,
+//       borderSide: BorderSide(
+//         color: Colors.transparent,
+//         width: 0,
+//       ),
+//     ),
+//   ),
+// )
+
+//   }
+// }
+
